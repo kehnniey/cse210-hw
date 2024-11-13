d68b47b1355e11b839a6d72339762c0c1d1c32c1
kehnniey <kehnniey@gmail.com>
2024-11-13 18:51:03 -0500

    learning activities

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job { _jobTitle = "Software Engineer", _company = "Microsoft", _startYear = 2019, _endYear = 2022 };
+        Job job2 = new Job { _jobTitle = "Manager", _company = "Apple", _startYear = 2022, _endYear = 2023 };
+
+        Resume resume = new Resume { _name = "Allison Rose" };
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
